628da0c4ba7e64b01c0887c81dfa2b6536772aa4
valeraqwe <valera.komar.1999@gmail.com>
2023-10-04 16:56:53 +0300

    Added models and defined relationships

---
+++ app/Models/Salon.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Salon extends Model
+{
+    use HasFactory;
+
+    public function services(): HasMany
+    {
+        return $this->hasMany(Service::class);
+    }
+
+}
